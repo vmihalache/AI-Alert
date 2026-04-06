771854c71f48ac0be7fe9c3a0e8cfa0b1d994bbe
valentin <mihalache.valentinflorian@gmail.com>
2026-04-06 19:13:57 +0300

    changed the raw to accept a stateCode instead of all

--- src/Ingestion.ts
+++ src/Ingestion.ts
@@ -2,10 +2,14 @@
 import  {NWSAlert} from '../types/sharedTypes'; // Adjust the path based on where NWSAlert is defined
 import {Repository} from './repository/schema'; // Adjust the path based on where Reposito
 export class Ingestion {  
-
+  stateCode: string;
+  constructor (stateCode: string) {
+    this.stateCode = stateCode;
+  }
   async ingestDataFromWeatherAPi () {
-    
-  const raw = await fetch('https://api.weather.gov/alerts/active');
+  
+
+  const raw = await fetch(`https://api.weather.gov/alerts/${this.stateCode}`);
   const transformed = await raw.json(); 
   
   // The weather API wraps the list in a "features" array
