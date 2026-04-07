3a05d7f7b93ddccebf263a64121793460333f62e
valentin <mihalache.valentinflorian@gmail.com>
2026-04-07 09:18:14 +0300

    changed seed.ts import

--- src/seed.ts
+++ src/seed.ts
@@ -1,4 +1,4 @@
-import {Repository} from './repository/schema.ts' // Adjust the path based on where Reposito
+import {Repository} from './repository/schema' // Adjust the path based on where Reposito
  const seedDbWithStates = async () => {
     await Repository.seedDbWithStates();
     }
