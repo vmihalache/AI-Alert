9a235d91e4f4be1f82b12d77025f613239dded5f
valentin <mihalache.valentinflorian@gmail.com>
2026-04-14 17:23:11 +0300

    changed the transport layer call in the client

--- src/mcp/mcpClient.ts
+++ src/mcp/mcpClient.ts
@@ -2,16 +2,16 @@ import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
 
 export class MCPClient {
-    serverAddress: string;
+    serverAddress: Promise<void>
     transport: StdioClientTransport;
     
     constructor(
-        serverAddress: string = ""
+        serverAddress: Promise<void> = Promise.resolve()
     ) {
         this.serverAddress = serverAddress;
         this.transport = new StdioClientTransport({
-            command: "node",
-            args: [this.serverAddress],
+            command: "npx",
+            args: ["tsx", "./src/mcp/mcpServer.ts"],
         });
     }
 
