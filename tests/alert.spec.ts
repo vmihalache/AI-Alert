d0824e0eb06ab40bf1bdbc56190208ca83e7ec80
valentin <mihalache.valentinflorian@gmail.com>
2026-05-05 12:01:41 +0300

    spec changes and enhancements

--- tests/alert.spec.ts
+++ tests/alert.spec.ts
@@ -2,8 +2,17 @@ import {test, expect} from "@playwright/test";
 import {mcpClient} from "../src/mcp/mcpClient";
 
 test('Weather Flow', async () => {
+  test.setTimeout(300000);
   await mcpClient.connection();
-  const result = await mcpClient.executeOrchestratedFlow({ type: "init", question: "What is the weather in Washington DC?" })
+  const location = "Virginia"
+  const question = `What is the weather in ${location}?`
+  const weatherKeywords = ["alert", "warning", "weather", "forecast", "temperature", "conditions", "active",  "fire", "wildfire", "danger", "risk", "humidity", "wind", "burn", "ignite"]
+  const result = await mcpClient.executeOrchestratedFlow({ type: "init", question: question })
+  const containsWeatherKeyword = weatherKeywords.some(keyword => result.message?.content?.toLowerCase().includes(keyword))
   console.log("result")
   console.log(result);
-});       
+  expect(result).toBeDefined() 
+  expect(result.message?.content?.length).toBeGreaterThan(0)
+  expect(containsWeatherKeyword).toBe(true)
+  expect(result.message?.content).toContain(location)
+},);       
